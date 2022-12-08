7897e19313a8ae2e0f6c22722b816ac383520147
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2022-12-08 11:23:41 +0100

    Added customer equality

--- backend/src/main/java/com/asedelivery/backend/Models/Customer.java
+++ backend/src/main/java/com/asedelivery/backend/Models/Customer.java
@@ -4,7 +4,16 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document("customer")
 public class Customer extends Person {
-    public Customer(String name, String password, String email){
+    public Customer(String name, String password, String email) {
         super(name, password, email);
+    }
+
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        } else if (obj instanceof Customer) {
+            Customer customer = (Customer) obj;
+            return customer.getId().equals(getId());
+        } else return false;
     }
 }
