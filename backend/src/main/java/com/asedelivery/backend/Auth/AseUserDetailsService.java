958cadc5bc292e2e901a6de90679402ca956db20
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2022-12-29 18:32:25 +0000

    Added AseUserDetailsService

---
+++ backend/src/main/java/com/asedelivery/backend/Auth/AseUserDetailsService.java
@@ -0,0 +1,33 @@
+package com.asedelivery.backend.Auth;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.asedelivery.backend.Models.Agent;
+import com.asedelivery.backend.Models.Principal;
+import com.asedelivery.backend.Models.Repositories.AgentRepository;
+import com.asedelivery.backend.Models.Repositories.PrincipalRepository;
+
+public class AseUserDetailsService implements UserDetailsService {
+    @Autowired
+    private PrincipalRepository principalRepo;
+
+    @Autowired
+    private AgentRepository agentRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Principal principal = principalRepo.findByUsername(username);
+
+        Agent agent = agentRepo.findByUsername(username);
+
+        return User.withDefaultPasswordEncoder()
+                .username(principal.getUsername())
+                .password(principal.getPassword())
+                .roles(agent.getRole())
+                .build();
+    }
+}
--- backend/src/main/java/com/asedelivery/backend/Auth/SecurityConfig.java
+++ backend/src/main/java/com/asedelivery/backend/Auth/SecurityConfig.java
@@ -1,9 +1,10 @@
-package com.asedelivery.backend.Config;
+package com.asedelivery.backend.Auth;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -21,7 +22,12 @@ public class SecurityConfig {
                 .httpBasic()
                 .and()
                 .sessionManagement().disable();
-                
+
         return http.build();
     }
+
+    @Bean
+	public UserDetailsService userDetailsService() {
+		return new AseUserDetailsService();
+	}
 }
