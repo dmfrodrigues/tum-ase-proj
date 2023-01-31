c76f6bcbdc18df7b4e61baf8e01f0985f3dcc046
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2023-01-31 15:20:39 +0100

    Stuff

--- backend/src/main/java/com/asedelivery/backend/Auth/SecurityConfig.java
+++ backend/src/main/java/com/asedelivery/backend/Auth/SecurityConfig.java
@@ -2,9 +2,12 @@ package com.asedelivery.backend.Auth;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,8 +19,9 @@ public class SecurityConfig {
                 .formLogin()
                 .and()
                 .csrf().disable()
-                .cors().disable()
-                .authorizeHttpRequests().requestMatchers("/**").authenticated()
+                .authorizeHttpRequests()
+                    .requestMatchers("/**").authenticated()
+                    .requestMatchers("/auth/**").permitAll()
                 .and()
                 .httpBasic()
                 .and()
@@ -30,4 +34,14 @@ public class SecurityConfig {
 	public UserDetailsService userDetailsService() {
 		return new AseUserDetailsService();
 	}
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
