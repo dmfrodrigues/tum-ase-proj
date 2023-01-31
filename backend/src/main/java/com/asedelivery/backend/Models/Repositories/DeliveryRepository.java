09388b984ffe821187d468040f280ace3d4f7da0
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2023-01-31 20:01:14 +0100

    Added find by deliverer and customer in delivery

--- backend/src/main/java/com/asedelivery/backend/Models/Repositories/DeliveryRepository.java
+++ backend/src/main/java/com/asedelivery/backend/Models/Repositories/DeliveryRepository.java
@@ -1,9 +1,17 @@
 package com.asedelivery.backend.Models.Repositories;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import com.asedelivery.backend.Models.Customer;
+import com.asedelivery.backend.Models.Deliverer;
 import com.asedelivery.backend.Models.Delivery;
 
 public interface DeliveryRepository extends MongoRepository<Delivery, String> {
+
+    List<Delivery> findByDeliverer(Deliverer deliverer);
+
+    List<Delivery> findByCustomer(Customer customer);
 
 }
