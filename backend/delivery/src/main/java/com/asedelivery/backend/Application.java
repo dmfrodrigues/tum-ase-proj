8d9d7067f834b37f989d53c205d72821aec22edc
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2023-02-03 20:08:16 +0100

    Added global config

--- backend/delivery/src/main/java/com/asedelivery/backend/Application.java
+++ backend/delivery/src/main/java/com/asedelivery/backend/Application.java
@@ -2,10 +2,11 @@ package com.asedelivery.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.Import;
+import com.asedelivery.common.config.AseLibAutoConfig;
 
 @SpringBootApplication
-@RestController
+@Import(AseLibAutoConfig.class)
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
