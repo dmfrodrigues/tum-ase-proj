85ecb15d3ef420c7f6c092090336400486518958
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2023-02-05 01:24:59 +0100

    Stuff

---
+++ frontend/build_push.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker build -t gitlab.lrz.de:5005/ase-22-23/team15/tum-ase-proj/frontend:latest frontend --target frontend-prod
+docker push gitlab.lrz.de:5005/ase-22-23/team15/tum-ase-proj/frontend:latest
